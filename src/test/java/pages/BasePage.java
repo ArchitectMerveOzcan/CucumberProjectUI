b1c536787d51f37a0bfa238069a36c731150e31d
Merve Özcan <ozcanmerve.tr@gmail.com>
2023-03-23 11:57:28 +0300

    repository

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,36 @@
+package pages;
+
+import Utilities.Driver;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import stepDefinitions.Hooks;
+@Slf4j
+public abstract class BasePage extends Hooks {
+    public BasePage() {
+        PageFactory.initElements(Driver.get(env), this);
+    }
+
+
+    public void clickWithWait(WebElement element){
+        long start = System.currentTimeMillis();
+        wait.until(ExpectedConditions.elementToBeClickable(element)).click();
+        log.info("Clicked in {}ms", System.currentTimeMillis() - start);
+    }
+
+
+    public void sleep(int millisec) throws InterruptedException {
+        Thread.sleep(millisec);
+    }
+
+    public void sendKey(WebElement element, String input){
+        long start = System.currentTimeMillis();
+        wait.until(ExpectedConditions.visibilityOf(element)).sendKeys(input);
+        log.info("Clicked in {}ms", System.currentTimeMillis() - start);
+    }
+
+    public boolean isDisplayed(WebElement element){
+        return wait.until(ExpectedConditions.visibilityOf(element)).isDisplayed();
+    }
+}
---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,18 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends BasePage{
+    @FindBy(id = "account-menu")
+    public WebElement userButton;
+
+    @FindBy (xpath = "//span[contains(text(),'Register')]")
+    public WebElement homePageDropDownRegisterButton;
+
+    public HomePage(){
+        PageFactory.initElements(driver, this);
+    }
+
+}
