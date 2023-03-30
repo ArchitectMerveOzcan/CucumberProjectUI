6fcac0ebfe78a3c8f3e60d24d5e3d20a52609a0d
Afatsum2 <musaksac@gmail.com>
2023-03-30 11:46:46 +0300

    Us5

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -5,11 +5,24 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends BasePage{
-    @FindBy(id = "account-menu")
+
+    @FindBy (id = "account-menu")
     public WebElement userButton;
+
+    @FindBy (id="login-item")
+    public WebElement loginButton;
 
     @FindBy (xpath = "//span[contains(text(),'Register')]")
     public WebElement homePageDropDownRegisterButton;
+
+    @FindBy (id= "login-item")
+    public WebElement SignInButton;
+
+
+
+    @FindBy(xpath = "//span[contains(text(),'Sign in')]")
+    public WebElement homePageSignInButton;
+
 
     public HomePage(){
         PageFactory.initElements(driver, this);
